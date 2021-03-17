320365743f5e8360082c87364e6f806933a3f6fe
Willian T <gomes.dream@gmail.com>
2021-03-17 15:28:42 -0300

    Versão inicial

---
+++ src/main/java/br/com/portfolio/demoPipeline/DemoPipelineApplication.java
@@ -0,0 +1,13 @@
+package br.com.portfolio.demoPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPipelineApplication.class, args);
+	}
+
+}
